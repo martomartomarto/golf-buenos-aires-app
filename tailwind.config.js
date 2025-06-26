e822426a2c3f6c6bd1f60217cd81335bbb7de261
martomartomarto <64854717+martomartomarto@users.noreply.github.com>
2025-06-26 13:32:22 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-{}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./data/**/*.{js,json}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
