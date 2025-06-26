210c57284e7b7bfa6cb4c7ca42fcaf53785ba2c5
martomartomarto <64854717+martomartomarto@users.noreply.github.com>
2025-06-26 20:29:32 -0300

    Update scores.tsx

--- pages/scores.tsx
+++ pages/scores.tsx
@@ -5,13 +5,23 @@ export default function ScoresPage() {
   return (
     <>
       <Head>
-        <title>Scores de Jugadores</title>
+        <title>Cargar Score</title>
       </Head>
 
       <main className="min-h-screen p-10 bg-white bg-opacity-90 font-poppins">
-        <h1 className="text-3xl font-bold mb-6 text-center">Scores cargados</h1>
-        
-        {/* Acá se muestra el formulario */}
+        <div className="mb-6">
+          <a
+            href="/"
+            className="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded"
+          >
+            ← Volver al inicio
+          </a>
+        </div>
+
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          🏌️ Do not cheat you lame ass player 🏌️‍♀️
+        </h1>
+
         <ScoreForm />
       </main>
     </>
