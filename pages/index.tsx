7e45fc3de16a05ae37b8bfdbd5a8eb64e3c769ca
martomartomarto <64854717+martomartomarto@users.noreply.github.com>
2025-06-26 13:26:11 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,19 @@ import courses from '../data/golf-courses.enriched.json';
 const Home = () => {
   return (
     <div
-      className="min-h-screen bg-cover bg-center p-4"
+      className="min-h-screen bg-fixed bg-center bg-cover bg-no-repeat"
       style={{ backgroundImage: "url('/Augusta.jpg')" }}
     >
-      <h1 className="text-4xl font-bold text-white mb-6 text-center drop-shadow-md">
-        Canchas de Golf en Buenos Aires
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {courses.map((course, index) => (
-          <GolfCourseCard key={index} course={course} />
-        ))}
+      <div className="backdrop-brightness-90 min-h-screen px-4 py-8">
+        <h1 className="text-4xl font-bold text-white mb-10 text-center drop-shadow-lg">
+          Canchas de Golf en Buenos Aires
+        </h1>
+
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {courses.map((course, index) => (
+            <GolfCourseCard key={index} course={course} />
+          ))}
+        </div>
       </div>
     </div>
   );
