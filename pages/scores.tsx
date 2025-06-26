83707568ca029a105ee1ceda1998e51abae03d2c
martomartomarto <64854717+martomartomarto@users.noreply.github.com>
2025-06-26 19:59:09 -0300

    Update scores.tsx

--- pages/scores.tsx
+++ pages/scores.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import ScoreForm from '../components/ScoreForm';
 
 export default function ScoresPage() {
   return (
@@ -9,9 +10,10 @@ export default function ScoresPage() {
 
       <main className="min-h-screen p-10 bg-white bg-opacity-90 font-poppins">
         <h1 className="text-3xl font-bold mb-6 text-center">Scores cargados</h1>
-        <p className="text-center">(Acá podríamos listar los scores de la Google Sheet en tiempo real.)</p>
+        
+        {/* Acá se muestra el formulario */}
+        <ScoreForm />
       </main>
     </>
   );
 }
-
