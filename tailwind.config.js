c0a11ced8a16fc6a5a287eaeffa854a236abe27a
martomartomarto <64854717+martomartomarto@users.noreply.github.com>
2025-06-28 14:07:16 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
+      // Definimos la nueva paleta de colores inspirada en el Masters
       colors: {
-        greenGlass: 'rgba(34,139,34,0.7)', // verde translúcido
+        masters: {
+          green: '#006747', // El verde clásico del Masters
+          yellow: '#FDB927', // El amarillo para acentos y botones
+          light: '#F5F5F5', // Un fondo de página blanco roto/beige claro
+          dark: '#333333', // El color principal para el texto
+        },
+      },
+      // Definimos las nuevas familias de fuentes
+      fontFamily: {
+        // Usaremos una fuente Serif elegante para los títulos
+        serif: ['"Playfair Display"', 'serif'],
+        // Y una fuente Sans-serif limpia para el cuerpo del texto
+        sans: ['"Source Sans 3"', 'sans-serif'],
       },
     },
   },
