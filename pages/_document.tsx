1fa8ca76260ab94b17e19e805c319109d6f6392f
martomartomarto <64854717+martomartomarto@users.noreply.github.com>
2025-06-28 14:07:37 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,14 +2,18 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="es">
       <Head>
+        {/* Importamos las nuevas fuentes desde Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Source+Sans+3:wght@400;600&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <body className="font-poppins">
+      {/* Aplicamos la fuente principal al cuerpo de la app */}
+      <body className="font-sans">
         <Main />
         <NextScript />
       </body>
